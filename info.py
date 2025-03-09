8208bad41719d57dac9ac39d72b2862699a241f0
Swar73 <swaryadav18@gmail.com>
2025-03-09 14:09:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,7 +76,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+Oz6dJo6obZRhMWE1') # S
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
 PM_SEARCH = bool(environ.get('PM_SEARCH', False))
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
